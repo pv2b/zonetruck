9191fb2562bc1e85b0866be3177b001667305093
pv2b <pvz@pvz.pp.se>
2017-10-05 21:39:45 +0200

    Update __init__.py

--- zonetruck/__init__.py
+++ zonetruck/__init__.py
@@ -3,4 +3,4 @@ import dns.resolver
 def main():
 	answers = dns.resolver.query('dnspython.org', 'MX')
 	for rdata in answers:
-    		print 'Host', rdata.exchange, 'has preference', rdata.preference
+    		print ('Host', rdata.exchange, 'has preference', rdata.preference)
